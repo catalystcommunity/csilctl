c7a42ed1730abf2e4105f7fd62d0021604a25d1e
JuniPuff <juniper@mothwithalamp.com>
2026-08-26 11:59:39 -0400

    fix: should fail tests

--- cli/src/list.rs
+++ cli/src/list.rs
@@ -393,6 +393,10 @@ mod tests {
         }
         out
     }
+    #[test]
+    fn should_fail() {
+        assert_eq!(true, false);
+    }
 
     #[test]
     fn resolve_follows_ident_chain_to_primitive() {
